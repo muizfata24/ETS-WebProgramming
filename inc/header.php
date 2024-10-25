8d6f73d04a8c3af7ec326cc3a87570515c99cbfd
rabidhermawan <97014247+rabidhermawan@users.noreply.github.com>
2024-10-25 14:42:46 +0700

    Change color

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <header class="header">
-	<h2 class="u-name">Task <b>Management</b>
+	<h2 class="u-name">PT Jaya Budiman</b>
 		<label for="checkbox">
 			<i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
 		</label>
